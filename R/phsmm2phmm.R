ae67ac052b4a8829d5647ebc862cf494e29fab64
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2023-05-17 13:40:38 +0200

    Add files via upload

---
+++ R/phsmm2phmm.R
@@ -0,0 +1,52 @@
+#' Converting periodic hsmm to periodic hmm
+#'
+#' @param omega
+#' @param dm
+#' @param eps
+#'
+#' @return
+#' @export
+#'
+#' @examples
+phsmm2phmm = function(omega,dm,eps=1e-25){
+  # dm is now a list (in time) of lists (over states)
+  L = length(dm) # length of one cycle
+  mv = sapply(dm[[1]],length) # lengths of the pmf vectors
+  m = length(mv) # number of state aggregates
+  G_all = vector("list")
+  F = vector("list") # computing all cdfs
+  for(k in 1:L){
+    Fk = vector("list")
+    for (i in 1:m){ Fk[[i]] = cumsum(c(0,dm[[k]][[i]][-mv[i]])) }
+    F[[k]] = Fk
+  }
+  for (t in 1:L){ # loop over time
+    G = matrix(0,0,sum(mv)) # constructing an empty matrix with number of columns equal to final dimension (sum_i m_i)
+    for (i in 1:m){ # loop over states
+      mi = mv[i] # length of pmf for state aggregate i
+      ci = numeric(mi)
+      for (k in 0:(mi-1)){
+        l = ifelse(t==k, L, (t-k)%%L)
+        l = ifelse(l==0, L, l)
+        ci[k+1] = ifelse(abs(1-F[[l]][[i]][k+1])>eps,dm[[l]][[i]][k+1]/(1-F[[l]][[i]][k+1]),1)
+      }
+      cim = ifelse(1-ci>0,1-ci,0)
+      Gi = matrix(0,mi,0)
+      for (j in 1:m){
+        if(i==j) {
+          if(mi==1){
+            Gi = cbind(Gi,c(rep(0,mv[[j]]-1),cim))
+          } else{
+            Gi = cbind(Gi,rbind(cbind(rep(0,mi-1),diag(cim[-mi],mi-1,mi-1)),
+                                c(rep(0,mi-1),cim[[mi]])))}
+        } else   { if(mi==1)
+        { Gi = cbind(Gi,matrix(c(omega[[i,j]]*ci,rep(0,mv[[j]]-1)),1))} else
+        { Gi = cbind(Gi,cbind(omega[[i,j]]*ci,matrix(0,mv[[i]],mv[[j]]-1)))}
+        }
+      }
+      G = rbind(G,Gi)
+    }
+    G_all[[t]] = G
+  }
+  G_all
+}
---
+++ R/pv.R
@@ -0,0 +1,28 @@
+#' Convenient computation of trigonometric predictor
+#'
+#' @param coef Matrix of beta coefficients
+#' @param time timepoint to evaluate
+#' @param degree number of trigonometric functions to use
+#' @param L length of one period
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pv = function(coef, time, degree, L = 24){
+  if(degree == 1){
+    return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*cos(2*pi*time/L))
+  } else if(degree == 2){
+    return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*sin(2*pi*time*2/L) +
+             coef[,4]*cos(2*pi*time/24) + coef[,5]*cos(2*pi*time*2/24))
+  } else if(degree == 3){
+    return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*sin(2*pi*time*2/L) + coef[,4]*sin(2*pi*time*3/L) +
+             coef[,5]*cos(2*pi*time/L) + coef[,6]*cos(2*pi*time*2/L) + coef[,7]*cos(2*pi*time*3/L))
+  } else if(degree == 4){
+    return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*sin(2*pi*time*2/L) + coef[,4]*sin(2*pi*time*3/L) + coef[,5]*sin(2*pi*time*4/L) +
+             coef[,6]*cos(2*pi*time/L) + coef[,7]*cos(2*pi*time*2/L) + coef[,8]*cos(2*pi*time*3/L) + coef[,9]*cos(2*pi*time*4/L))
+  } else if(degree == 5){
+    return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*sin(2*pi*time*2/L) + coef[,4]*sin(2*pi*time*3/L) + coef[,5]*sin(2*pi*time*4/L) + coef[,6]*sin(2*pi*time*5/L) +
+             coef[,7]*cos(2*pi*time/L) + coef[,8]*cos(2*pi*time*2/L) + coef[,9]*cos(2*pi*time*3/L) + coef[,10]*cos(2*pi*time*4/L) + coef[,11]*cos(2*pi*time*5/L))
+  }
+}
