6c933b12262c1f14acf5840579bf2aa38f96b7b1
Jan-Ole Koslik <janole.koslik@gmail.com>
2023-06-28 15:57:46 +0200

    error correction

--- R/pv.R
+++ R/pv.R
@@ -14,7 +14,7 @@ pv = function(coef, time, degree, L = 24){
     return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*cos(2*pi*time/L))
   } else if(degree == 2){
     return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*sin(2*pi*time*2/L) +
-             coef[,4]*cos(2*pi*time/24) + coef[,5]*cos(2*pi*time*2/24))
+             coef[,4]*cos(2*pi*time/L) + coef[,5]*cos(2*pi*time*2/L))
   } else if(degree == 3){
     return(coef[,1] + coef[,2]*sin(2*pi*time/L) + coef[,3]*sin(2*pi*time*2/L) + coef[,4]*sin(2*pi*time*3/L) +
              coef[,5]*cos(2*pi*time/L) + coef[,6]*cos(2*pi*time*2/L) + coef[,7]*cos(2*pi*time*3/L))
